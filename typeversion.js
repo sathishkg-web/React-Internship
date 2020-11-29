53b4254765a7a371398879f90f9e19c7c7abec26
sathishkg-web <59190276+sathishkg-web@users.noreply.github.com>
2020-11-29 10:13:53 +0530

    Create typeversion.js

---
+++ typeversion.js
@@ -0,0 +1,9 @@
+function getBrowserDetail() {
+  alert(navigator.appName); // To see the Browser Application Name
+
+  alert(navigator.product); // To see the Browser Application Engine
+
+  alert(navigator.appVersion); // To see the Browser Application Version
+}
+
+getBrowserDetail();
