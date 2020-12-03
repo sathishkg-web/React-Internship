0dd17a71eed0bc3400db94b5d1efb957e65cab9e
sathishkg-web <59190276+sathishkg-web@users.noreply.github.com>
2020-12-04 02:05:50 +0530

    Create day6_task1.js

---
+++ day6_task1.js
@@ -0,0 +1,14 @@
+Array.prototype.befindobject = function (propertyName) {
+  const propertyValue = [];
+  this.map(function (object) {
+    propertyValue.push(object[propertyName]);
+  });
+  return propertyValue;
+};
+
+var sampleinput = [
+  { name: "John", age: 26 },
+  { name: "siva", age: 27 },
+];
+
+console.log(sampleinput.befindobject("name"));
