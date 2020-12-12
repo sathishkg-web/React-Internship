8c9cd63f7aafab29573e2d01a21169254f725dc9
sathishkg-web <59190276+sathishkg-web@users.noreply.github.com>
2020-12-12 10:20:30 +0530

    Update App.js

--- Day13task/src/App.js
+++ Day13task/src/App.js
@@ -1,1 +1,29 @@
-import
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '' };
+  }
+  handleClick = (event) => {
+    alert("Get Value: " + this.state.username);
+  }
+  myChangeHandler = (event) => {
+    this.setState({username: event.target.value});
+  }
+  render() {
+    return (
+      
+     <div>
+      <p>Enter your name</p>
+      <input
+        type='text'
+        onChange={this.myChangeHandler}
+      />
+      <input type='button' value="get value" onClick={this.handleClick}
+        
+      />
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
