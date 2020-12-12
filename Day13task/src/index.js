461b40f55e108bbb8fc05958ed5fa4f58446113c
sathishkg-web <59190276+sathishkg-web@users.noreply.github.com>
2020-12-12 10:34:29 +0530

    Update index.js

--- Day13task/src/index.js
+++ Day13task/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import MyComponent from './App';
 
-ReactDOM.render(<App />,
+ReactDOM.render(<Mycomponent />,
   document.getElementById('root')
 );
 
